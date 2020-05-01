66ba31c29e962b8aa134439939d0f5e2545efc9e
Andrey Platov <andrey.v.platov@gmail.com>
2020-05-01 05:59:32 +0200

    Fix comments
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- plugins/platform-core/src/__tests__/reflect.test.ts
+++ plugins/platform-core/src/__tests__/reflect.test.ts
@@ -11,7 +11,6 @@
 // 
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
 //
 
 import { Model, getClassMetadata } from '../__model__/reflect'
--- plugins/platform-core/src/index.ts
+++ plugins/platform-core/src/index.ts
@@ -1,4 +1,4 @@
-//
+/*! Antierp Platform
 // Copyright © 2020 Andrey Platov <andrey.v.platov@gmail.com>
 // 
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
@@ -11,7 +11,6 @@
 // 
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
-//
+*/
 
 console.log('index')
