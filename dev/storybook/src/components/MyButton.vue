154c98e807beb82d1cfd27f38d7539081d67a156
Andrey Platov <andrey.v.platov@gmail.com>
2020-05-03 21:44:50 +0200

    chore: create storybook
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

---
+++ dev/storybook/src/components/MyButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button is-primary" @click="onClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'my-button',
+
+  methods: {
+    onClick() {
+      this.$emit('click')
+    }
+  }
+})
+</script>
+
+<style scoped>
+button {
+  border: 1px solid #eee;
+  border-radius: 3px;
+  background-color: #ffffff;
+  cursor: pointer;
+  font-size: 15pt;
+  padding: 3px 10px;
+  margin: 10px;
+}
+</style>
