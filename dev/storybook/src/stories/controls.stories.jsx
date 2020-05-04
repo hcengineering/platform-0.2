d34ca7e36dbda46755a5f0d9c98eba186b28fb3d
Andrey Platov <andrey.v.platov@gmail.com>
2020-05-04 04:16:30 +0200

    feat(controls): add `InlineEdit`
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- dev/storybook/src/stories/controls.stories.jsx
+++ dev/storybook/src/stories/controls.stories.jsx
@@ -1,7 +1,9 @@
 import { action } from '@storybook/addon-actions'
 
 import Theme from '../components/Theme.vue'
+
 import Button from '@anticrm/platform-ui-controls/src/Button.vue'
+import InlineEdit from '@anticrm/platform-ui-controls/src/InlineEdit.vue'
 
 export default {
   title: 'Controls'
@@ -10,5 +12,11 @@ export default {
 export const button = () => ({
   render() {
     return <Theme><Button on-click={action('clicked')}>Hello Button</Button></Theme>
+  }
+})
+
+export const inlineEdit = () => ({
+  render() {
+    return <Theme><InlineEdit placeholder="Placeholder..."></InlineEdit></Theme>
   }
 })
