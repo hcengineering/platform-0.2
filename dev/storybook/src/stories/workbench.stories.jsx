0b5db622d728a7fdd852507bc42d264cd8f5fa83
Andrey Platov <andrey.v.platov@gmail.com>
2020-05-15 09:28:17 +0200

    feat: initial `Workbench` contribution
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

---
+++ dev/storybook/src/stories/workbench.stories.jsx
@@ -0,0 +1,37 @@
+//
+// Copyright © 2020 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Theme from '../components/Theme.vue'
+
+import ui from '@anticrm/platform-ui'
+import platform from '@anticrm/platform'
+
+import Workbench from '@anticrm/platform-workbench/src/components/Workbench.vue'
+
+import core from '@anticrm/platform-core'
+
+export default {
+  title: 'Workbench'
+}
+
+const corePlugin = platform.getPlugin(core.id)
+const session = corePlugin.getSession()
+
+export const workbench = () => ({
+  render() {
+    return <Workbench />
+  }
+})
+
---
+++ plugins/platform-workbench/src/components/Workbench.vue
@@ -0,0 +1,102 @@
+<!--
+// Copyright © 2020 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+
+<template>
+  <div id="workbench">
+    <header>
+      <!-- <Header @add="addObject()" /> -->
+    </header>
+
+    <nav>
+      <!-- <Sidenav :applications="applications" /> -->
+    </nav>
+
+    <main>
+      <router-view></router-view>
+    </main>
+
+    <aside>
+      <!-- <FormEditor v-if="leftPane" :object="leftPane" @save="onSave()"></FormEditor> -->
+    </aside>
+
+    <footer></footer>
+  </div>
+</template>
+
+<style lang="scss">
+@import "~@anticrm/platform-ui-theme/css/_variables.scss";
+
+#workbench {
+  display: grid;
+
+  grid-template-columns: $pictogram-size 250px 1fr auto;
+  grid-template-rows: $pictogram-size 1fr 24px;
+
+  height: 100vh;
+
+  header {
+    grid-column-start: 1;
+    grid-column-end: 5;
+
+    grid-row-start: 1;
+    grid-row-end: 2;
+
+    background-color: $header-bg-color;
+    border-bottom: 1px solid $workspace-separator-color;
+  }
+
+  nav {
+    grid-column-start: 1;
+    grid-column-end: 2;
+
+    grid-row-start: 2;
+    grid-row-end: 3;
+
+    background-color: $nav-bg-color;
+  }
+
+  main {
+    grid-column-start: 2;
+    grid-column-end: 4;
+
+    grid-row-start: 2;
+    grid-row-end: 3;
+
+    background-color: $content-bg-color;
+    padding: 0em 1em;
+  }
+
+  aside {
+    grid-column-start: 4;
+    grid-column-end: 5;
+
+    grid-row-start: 2;
+    grid-row-end: 3;
+
+    background-color: $header-bg-color;
+    border-left: 1px solid $workspace-separator-color;
+  }
+
+  footer {
+    grid-column-start: 1;
+    grid-column-end: 5;
+
+    grid-row-start: 3;
+    grid-row-end: 4;
+
+    background-color: purple;
+  }
+}
+</style>
