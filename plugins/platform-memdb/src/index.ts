49bde73ab69acc75967905ec1f5524626f88b874
Andrey Platov <andrey.v.platov@gmail.com>
2020-05-18 23:30:24 +0200

    feat: model experiments
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- plugins/platform-memdb/src/index.ts
+++ plugins/platform-memdb/src/index.ts
@@ -23,12 +23,13 @@ type NumberProperty<T> = number & Property<T>
 
 type PropertyType = Property<any> | undefined
 
-
 type Str = StringProperty<string>
 
 type Resource<T> = { __resource: T }
 
 type Ref<T> = StringProperty<T> & { __ref: true }
+
+type Bag<T extends PropertyType> = { [key: string]: T }
 
 interface Obj {
   _class: Ref<Class<this>>
@@ -36,30 +37,117 @@ interface Obj {
 
 interface Doc extends Obj {
   _id: Ref<this>
+  _mixins?: Ref<Class<Doc>>[]
 }
 
 interface Type<T> extends Obj {
 
 }
 
-interface Class<T extends Obj> extends Doc {
-  _attributes: { [key: string]: Type<any> }
+/////
+
+type AsDescrtiptors<T> = { [P in keyof T]:
+  T[P] extends Property<infer X> ? Type<X> :
+  T[P] extends { [key: string]: PropertyType } ? Type<Bag<PropertyType>> :
+  never
+}
+type Descriptors<T extends object> = AsDescrtiptors<T>
+export type Attributes<T extends E, E extends Obj> = Descriptors<Omit<T, keyof E>>
+
+interface EClass<T extends E, E extends Obj> extends Doc {
+  _attributes: Attributes<T, E>
 }
 
-type Instantiate<O> = {
-  [P in keyof O]: O[P] extends Property<infer T> ? T : never
+type Class<T extends Obj> = EClass<T, Obj>
+
+////////////
+
+// type Instantiate<O> = {
+//   [P in keyof O]: O[P] extends Property<infer T> ? T : never
+// }
+
+// function newInstance<I extends object, X extends { [key: string]: PropertyType }> (intf: Ref<I>, object: X): Instantiate<X> {
+//   return {} as Instantiate<X>
+// }
+
+/////
+
+interface Session {
+  // newInstance       <M extends T, T extends Doc>         (clazz: Ref<EClass<M, T>>, values: Omit<M, keyof T>): M 
+  //     newInstance === mixin, where D = Doc & T = Doc
+  // mixin<D extends T, M extends T, T extends Doc> (doc: D, clazz: Ref<EClass<M, T>>, values: Omit<M, keyof T>): M 
+
+  newInstance<M extends Obj> (clazz: Ref<Class<M>>, values: Omit<M, keyof Obj>): M
+  mixin<D extends T, M extends T, T extends Doc> (doc: D, clazz: Ref<EClass<M, T>>, values: Omit<M, keyof T>): M
 }
 
-function newInstance<I extends object, X extends { [key: string]: PropertyType }> (intf: Ref<I>, object: X): Instantiate<X> {
-  return {} as Instantiate<X>
+interface Person extends Doc {
+  firstName: Str
+  lastName: Str
 }
 
-interface Person {
-  firstName: string
-  lastName: string
-  age: number
-}
+const x = {} as EClass<Person, Doc>
 
-const refPerson = '' as Ref<Person>
+x._attributes.firstName
 
-const z = newInstance(refPerson, { a: '' as Str })
+const y = {} as Class<Class<Obj>>
+
+const refPerson = '' as Ref<Class<Person>>
+const refClassClass = '' as Ref<Class<EClass<Person, Doc>>>
+const refTypeString = '' as Ref<Class<Type<string>>>
+
+const S = {} as Session
+
+const z = S.newInstance(refPerson, { firstName: 'John' as Str, lastName: 'Carmack' as Str, _id: '' as Ref<Person> })
+// const v = S.newInstance(refClassClass, {
+//   _attributes: {
+//     firstName: S.newInstance(refTypeString, {}),
+//     lastName: S.newInstance(refTypeString, {})
+//   }
+// })
+
+//////
+
+// export const metaModel = [
+//   newContainer(core.class.Class, {
+//     _id: core.class.Emb,
+//     _native: core.native.Emb,
+//     _attributes: {}
+//   }),
+//   newContainer(core.class.Class, {
+//     _id: core.class.Doc,
+//     _native: core.native.Doc,
+//     _attributes: {
+//       _id: ref(core.class.Doc)
+//     }
+//   }),
+//   createStruct(core.class.Type, core.class.Emb, {}, core.native.Type),
+//   createStruct(core.class.Resource, core.class.Type, {}, core.native.Type),
+//   createStruct(core.class.Metadata, core.class.Type, {}, core.native.Type),
+//   createStruct(core.class.String, core.class.Type, {}, core.native.Type),
+
+//   createStruct(core.class.RefTo, core.class.Type, {
+//     to: ref(core.class.Class)
+//   }, core.native.Type),
+//   createStruct(core.class.BagOf, core.class.Type, {
+//     of: obj(core.class.Type)
+//   }, core.native.BagOf),
+//   createStruct(core.class.ArrayOf, core.class.Type, {
+//     of: obj(core.class.Type)
+//   }, core.native.ArrayOf),
+//   createStruct(core.class.InstanceOf, core.class.Type, {
+//     of: ref(core.class.Struct)
+//   }, core.native.InstanceOf),
+
+//   createClass(core.class.StructuralFeature, core.class.Doc, {
+//     _attributes: bag(obj(core.class.Type)),
+//     _extends: ref(core.class.Struct),
+//     _native: resource()
+//   }, core.native.StructuralFeature),
+
+//   createClass(core.class.Struct, core.class.StructuralFeature, {
+//   }, core.native.Struct),
+
+//   createClass(core.class.Class, core.class.StructuralFeature, {
+//   }, core.native.Class)
+// ]
