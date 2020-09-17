50b46068f3a98ad1d366bb223317fa283d688959
Andrey Sobolev <haiodo@xored.com>
2020-09-17 12:13:19 +0700

    Svelte Rich control
    In progress
    
    Signed-off-by: Andrey Sobolev <haiodo@xored.com>
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

---
+++ packages/sparkling-controls/src/toolbar/Button.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	export let disabled: boolean = false
+	export let selected: boolean = false
+	export let style: string = ''
+	export let clazz: string = ''
+</script>
+
+<button
+	type="button"
+	class="crm-toolbar-button small"
+	style="{style}"
+	class:selected
+	disabled="{disabled}"
+	on:click
+>
+	<slot />
+</button>
+
+<style lang="scss">
+	.crm-toolbar-button {
+		display: inline-block;
+		border: 0px solid currentColor;
+		border-radius: 2px;
+		padding: 0.5em 1em 0.5em;
+		box-sizing: border-box;
+		cursor: pointer;
+		user-select: none;
+		text-align: center;
+
+		font: inherit;
+		font-weight: 400;
+
+		color: inherit;
+		background-color: inherit;
+
+		&:focus {
+			outline: none;
+			border-color: var(--theme-highlight-color);
+			box-shadow: inset 0px 0px 2px 0px var(--theme--highlight-color);
+		}
+
+		&.large {
+			padding: 0.75em 1.5em 0.75em;
+		}
+		&.small {
+			padding: 0 0.6em 0;
+		}
+
+		&.selected {
+			background-color: var(--theme-highlight-color);
+			font-weight: bold;
+			// background-color: darken(--theme-highlight-color, 10%);
+		}
+
+		&:hover {
+			border-color: var(--theme-highlight-color);
+			// color: $highlight-color;
+			// background-color: $highlight-color;
+			// background-color: darken($highlight-color, 30%);
+		}
+	}
+</style>
---
+++ plugins/presentation/src/components/refinput/CompletionPopup.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+	import Icon from '@anticrm/platform-ui/src/components/Icon.svelte'
+
+	import ui from '@anticrm/platform-ui'
+	import { Class, Ref, VDoc } from '@anticrm/core'
+	import ScrollView from '@anticrm/sparkling-controls/src/ScrollView.svelte'
+
+	import { createEventDispatcher, onMount } from 'svelte'
+
+	import {
+		CompletionItem,
+		Position,
+		getFirst,
+		calcOffset,
+	} from './CompletionPopupHelper'
+
+	const dispatch = createEventDispatcher()
+
+	export let items: CompletionItem[] = []
+	export let pos: Position
+
+	let listElement: HTMLElement
+	let selElement: HTMLElement
+	let selOffset: number
+	let selection = getFirst(items)
+	let popupStyle = ''
+
+	function selectItem(item: string) {
+		dispatch('select', item)
+	}
+
+	$: {
+		popupStyle = `left: ${pos.left + 5}px; top: ${pos.top - 80}px;`
+
+		let cs = items.find((e) => e.key == selection.key)
+		if (cs == null) {
+			// Filtering caused selection to be wrong, select first
+			selection = getFirst(items)
+		}
+
+    selOffset = calcOffset(selElement)
+    console.log(items)
+	}
+
+	export function handleUp() {
+		let pos = items.indexOf(selection)
+		if (pos > 0) {
+			selection = items[pos - 1]
+		}
+	}
+	export function handleDown() {
+		let pos = items.indexOf(selection)
+		if (pos < items.length - 1) {
+			selection = items[pos + 1]
+		}
+	}
+	export function handleSubmit() {
+		dispatch('select', selection)
+	}
+</script>
+
+<div class="presentation-completion-popup" style="{popupStyle}">
+	<!-- <ScrollView stylez="height:100%;width: 100%;" scrollPosition="{selOffset}"> -->
+		<div bind:this="{listElement}">
+			{#each items as item (item.key)}
+				<div
+					class="item"
+					class:selected="{item.key == selection.key}"
+					on:click|preventDefault="{() => selectItem(item.key)}"
+				>
+					{#if item.key == selection.key}
+						<div
+							class="focus-placeholder"
+							bind:this="{selElement}"
+							style="width:0px"
+						></div>
+					{/if}
+					{item.title || item.label}
+				</div>
+			{/each}
+		</div>
+	<!-- </ScrollView> -->
+</div>
+
+<style lang="scss">
+	.presentation-completion-popup {
+		display: flex;
+		flex-direction: column;
+		background-color: var(--theme-input-color);
+		color: #fff;
+		position: absolute;
+		border: 1px solid var(--theme-content-color-dark);
+		border-radius: 3px;
+		height: 75px;
+		width: 300px;
+
+		.item {
+			font-size: 14px;
+			font-family: Raleway;
+			white-space: no-wrap;
+			width: 100%;
+
+			&.selected {
+				border-color:var(--theme-highlight-color);
+				background-color:var(--theme-highlight-color);
+				position: sticky;
+			}
+
+			&:focus {
+				outline: none;
+				border-color: var(--theme-highlight-color);
+				box-shadow: inset 0px 0px 2px 0px var(--theme-highlight-color);
+			}
+
+			&:hover {
+				border-color: var(--theme-highlight-color);
+				background-color: gsl(var(--theme-highlight-color), 80%);
+			}
+		}
+	}
+</style>
