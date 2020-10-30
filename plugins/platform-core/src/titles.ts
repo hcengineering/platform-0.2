c3c26ed49185c89bc630b4a6ed90bbde93cd4fb4
Andrey Sobolev <haiodo@gmail.com>
2020-10-30 19:25:58 +0700

    Fix empty title
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

--- plugins/platform-core/src/titles.ts
+++ plugins/platform-core/src/titles.ts
@@ -1,14 +1,14 @@
 //
 // Copyright © 2020 Anticrm Platform Contributors.
-// 
+//
 // Licensed under the Eclipse Public License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License. You may
 // obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 
+//
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
@@ -43,7 +43,7 @@ export class Titles implements Storage {
     }
     const prefix = query['title'] as string
     for (const node of this.graph.values()) {
-      if (node.title.startsWith(prefix) && this.implements(node, query._objectClass as Ref<Classifier<Doc>>)) {
+      if (node.title && node.title.startsWith(prefix) && this.implements(node, query._objectClass as Ref<Classifier<Doc>>)) {
         const title: Title = {
           _id: ('title_' + node._id) as Ref<Doc>,
           _class: core.class.Title,
