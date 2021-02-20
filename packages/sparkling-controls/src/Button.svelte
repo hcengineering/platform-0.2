4394d7d57053154a1712974195861e3e56912196
Alexander Platov <sas_lord@mail.ru>
2021-02-20 15:04:47 +0300

    SB add ThemeSelector, comp Button (#234)
    * SB add ThemeSelector, comp Button
    * update Button from SB

--- packages/sparkling-controls/src/Button.svelte
+++ packages/sparkling-controls/src/Button.svelte
@@ -1,42 +1,28 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
   // --- 'small', 'large' --- //
   export let size: string = ''
   // --- 'primary', 'transparent' --- //
   export let kind: string = ''
   // --- ex: '100px', '25%' --- //
   export let width: string = ''
+
+  const dispatch = createEventDispatcher();
+  function onClick(event) {
+    dispatch('click', event);
+  }
 </script>
 
 <button
   type="button"
   class="button {size} {kind}"
   style="{(width !== '') ? 'width:' + width : ''}"
-  on:click
+  on:click={onClick}
 >
   <slot />
 </button>
 
 <style lang="scss">
-  .transparent {
-    display: flex;
-    border: none;
-    cursor: pointer;
-    user-select: none;
-    font-weight: 500;
-    color: var(--theme-content-color);
-    background-color: transparent;
-    align-items: center;
-    justify-content: center;
-    flex-wrap: nowrap;
-    padding: 0;
-    margin: 0;
-
-    &:focus {
-      outline: none;
-    }
-
-    &:hover {
-      color: var(--theme-caption-color);
-    }
-  }
+  
 </style>
