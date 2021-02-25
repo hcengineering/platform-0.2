1ad48155a0e4ba915ae4a25b5be539c8d7a187b3
Alexander Platov <sas_lord@mail.ru>
2021-02-25 21:27:46 +0300

    add comp ActionBar in SB, fix in Platform
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

---
+++ dev/storybook/src/stories/sparkling-controls/ActionBar.stories.js
@@ -0,0 +1,54 @@
+import ActionBar from './ActionBar.svelte'
+import ThemeDecorator from '../ThemeDecorator.svelte'
+
+export default {
+  title: 'Platform/ActionBar',
+  component: ActionBar,
+  argTypes: {
+    actions: { control: 'array' },
+    onTop: { control: 'number' },
+  },
+  parameters: {
+    backgrounds: {
+      values: [
+        { name: 'light', value: '#fff' },
+        { name: 'dark', value: '#1E1E1E' },
+        { name: 'biege', value: '#FDF1E6' },
+      ],
+    },
+  },
+  decorators:  [(storyFn) => {
+    const story = storyFn();
+
+    return {
+      Component: ThemeDecorator,
+      props: {
+        child: story.Component,
+        props: story.props,
+      }
+    }
+  }]
+}
+
+const Template = ({ ...args }) => ({
+  Component: ActionBar,
+  props: { 
+    actions: [{
+      label: 'Action Item 1', action: () => alert('PopupItem1.Action()')
+    }, {
+      label: 'Action Item 2', action: () => alert('PopupItem2.Action()')
+    }, {
+      label: 'Action Item 3', action: () => alert('PopupItem3.Action()')
+    }, {
+      label: '-'
+    }, {
+      label: 'Action Item 4', action: () => alert('PopupItem4.Action()')
+    }],
+    ...args
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  onTop: 2,
+};
