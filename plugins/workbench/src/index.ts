f75aafbd3f360a39c17090e470b9f2f18f6daf45
Andrey Platov <andrey.v.platov@gmail.com>
2021-03-11 14:06:21 +0100

    make linter happy
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- plugins/workbench/src/index.ts
+++ plugins/workbench/src/index.ts
@@ -16,7 +16,7 @@
 import { plugin, Plugin, Service } from '@anticrm/platform'
 
 import core from '@anticrm/platform-core'
-import ui, { AnyComponent, Asset, Document } from '@anticrm/platform-ui'
+import ui, { AnyComponent, Asset } from '@anticrm/platform-ui'
 import { IntlString } from '@anticrm/platform-i18n'
 import { Class, Doc, Ref } from '@anticrm/core'
 import { Application, VDoc } from '@anticrm/domains'
