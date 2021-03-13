d2a9263b6b48d422058ea54afd933f7097dec063
Artyom Savchenko <artem.savchenko@xored.com>
2021-03-13 13:50:37 +0700

    Configure eslint for svelte files
    Signed-off-by: Artyom Savchenko <artem.savchenko@xored.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,12 +20,21 @@ module.exports = {
       '**/tsconfig.json',
     ]
   },
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ['@typescript-eslint', 'import', 'svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
   rules: {
     'no-unused-vars': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['error']
+  },
+  settings: {
+    'svelte3/typescript': require('typescript')
   }
 }
