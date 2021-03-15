4b27658d6ab9919fc83b6d1f310bd453b3b54013
artyom-savchenko <79742701+artyom-savchenko@users.noreply.github.com>
2021-03-15 15:23:23 +0700

    Configure eslint for svelte files (#284)
    * Configure eslint for svelte files
    * Ignore styles since SASS preprocessor is not supported
    * Add eslint for svelte in presentation project
    * Add svelte plugin for projects with enabled linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,12 +20,23 @@ module.exports = {
       '**/tsconfig.json',
     ]
   },
-  plugins: ['@typescript-eslint', 'import'],
+  plugins: ['@typescript-eslint', 'import', 'svelte3'],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
   rules: {
     'no-unused-vars': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ['error']
+  },
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    // Ignore styles since SASS preprocessor is not supported in svelte plugin: https://github.com/sveltejs/eslint-plugin-svelte3/issues/10
+    'svelte3/ignore-styles': () => true
   }
 }
