64ab7d58f99c93e6c7bc9e10b7cba94b17d50526
Andrey Sobolev <haiodo@gmail.com>
2021-03-23 22:54:16 +0700

    Data generator for tasks
    Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

---
+++ plugins/data-generator/src/components/internal/DataGeneratorView.svelte
@@ -0,0 +1,174 @@
+<!--
+// Copyright © 2020 Anticrm Platform Contributors.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+<script type="ts">
+
+  import { NumberProperty, Property, Ref, StringProperty } from '@anticrm/core'
+  import { WorkbenchApplication } from '@anticrm/workbench'
+  import { Application, CORE_CLASS_SPACE, CORE_MIXIN_SHORTID, Space } from '@anticrm/domains'
+
+  import ScrollView from '@anticrm/sparkling-controls/src/ScrollView.svelte'
+  import { createLiveQuery, getCoreService } from '@anticrm/presentation'
+  import Button from '@anticrm/sparkling-controls/src/Button.svelte'
+  import EditBox from '@anticrm/sparkling-controls/src/EditBox.svelte'
+  import ComboBox from '@anticrm/platform-ui/src/components/ComboBox.svelte'
+  import task, { TASK_STATUS_OPEN, TaskFieldType, TaskFieldValue } from '@anticrm/task'
+  import chunter, { Comment } from '@anticrm/chunter'
+  import { Action } from '@anticrm/platform-ui'
+
+  const faker = require('faker')
+
+  export let application: WorkbenchApplication
+
+  let taskCount = 50
+  let taskSpace: Space | undefined
+  let spaces: Space[] = []
+  let taskStatus: TaskFieldValue[] = []
+
+  createLiveQuery(CORE_CLASS_SPACE, {}, (docs) => {
+    spaces = docs
+  })
+
+  createLiveQuery(task.class.TaskFieldValue, { type: TaskFieldType.Status as NumberProperty }, (docs) => {
+    taskStatus = docs
+  })
+
+  function filterSpaces (spaces: Space[], app: Ref<Application>): Space[] {
+    return spaces.filter(sp => sp.application === app)
+  }
+
+  function getActions (spaces: Space[], app: Ref<Application>): Action[] {
+    return filterSpaces(spaces, app).map(sp => {
+      return {
+        id: sp._id,
+        name: sp.name,
+        action: () => {
+          taskSpace = sp
+        }
+      } as Action
+    })
+  }
+
+  const coreService = getCoreService()
+
+  async function generateTasks () {
+    if (!taskSpace) {
+      return
+    }
+    const cs = await coreService
+    for (let i = 0; i < taskCount; i++) {
+
+      let modelDb = cs.getModel()
+      const newTask = modelDb.newDoc(task.class.Task, cs.generateId(), {
+        title: faker.commerce.productName() as StringProperty,
+        _space: taskSpace._id,
+        status: taskStatus[(faker.random.number() as number) % taskStatus.length]._id,
+        comments: [{
+          message: faker.commerce.productDescription(),
+          _class: chunter.class.Comment,
+          _createdOn: Date.now() as Property<number, Date>,
+          _createdBy: cs.getUserId() as Property<string, string>
+        } as Comment]
+      })
+      try {
+        const asShortId = modelDb.cast(newTask, CORE_MIXIN_SHORTID)
+        asShortId.shortId = await cs.genRefId(taskSpace._id as Ref<Space>)
+      } catch (e) {
+        // Ignore
+        console.log(e)
+      }
+      console.log('NEW TASK', newTask)
+
+      await cs.create(task.class.Task, newTask)
+    }
+  }
+
+  async function removeTasks () {
+    const cs = await coreService
+    if (!taskSpace) {
+      return
+    }
+    const tasks = await cs.find(task.class.Task, { _space: taskSpace._id })
+    for (const t of tasks) {
+      await cs.remove(t, null)
+    }
+  }
+
+  $: {
+    if (!taskSpace) {
+      const sps = filterSpaces(spaces, task.application.Task)
+      if (sps && sps.length > 0) {
+        taskSpace = sps[0]
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .activity {
+    height: 100%;
+    // background-color: red;
+    display: flex;
+    flex-direction: column;
+
+    .content {
+      flex-grow: 1;
+    }
+
+    .actions {
+      display: flex;
+      flex-direction: row;
+    }
+
+    .captionContainer {
+      box-sizing: border-box;
+      width: 100%;
+      height: 5em;
+      padding: 2em;
+      border-bottom: 1px solid var(--theme-bg-accent-color);
+      display: flex;
+      align-items: center;
+    }
+  }
+</style>
+
+<div class="activity">
+  <div class="captionContainer">
+    <span class="caption-1">Data Generator</span>&nbsp;
+  </div>
+  <ScrollView height="100%" margin="2em" autoscroll={true}>
+    <div class="content">
+      Task Generator:
+      <div class="actions">
+        <div style="width: 300px">
+          <ComboBox label="Space" items={getActions(spaces, task.application.Task)}>
+            <div slot="title">
+              {#if taskSpace}
+                {taskSpace.name}
+              {/if}
+            </div>
+          </ComboBox>
+        </div>
+        <EditBox id="create_task__input__name" bind:value={taskCount} width="100%"
+                 label="Name" placeholder="Name" />
+
+        <div>
+          <Button width="100px" on:click={()=> generateTasks()}>Generate Tasks</Button>
+          <Button width="100px" on:click={()=> removeTasks()}>Remove All Tasks</Button>
+        </div>
+      </div>
+    </div>
+  </ScrollView>
+</div>
---
+++ plugins/data-generator/src/index.ts
@@ -0,0 +1,51 @@
+//
+// Copyright © 2020, 2021 Anticrm Platform Contributors.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import { plugin, Plugin, Service } from '@anticrm/platform'
+import { AnyComponent, Asset } from '@anticrm/platform-ui'
+
+import _core from '@anticrm/platform-core'
+import _task from '@anticrm/task'
+// import _chunter from '@anticrm/chunter'
+import _contact from '@anticrm/contact'
+import { Ref } from '@anticrm/core'
+import { Application } from '@anticrm/domains'
+
+// P L U G I N
+
+export interface DataGenService extends Service {
+}
+
+const dataGenPlugin = plugin(
+  'data-generator' as Plugin<DataGenService>,
+  {
+    core: _core.id,
+    task: _task.id,
+    // chunter: _chunter.id,
+    contact: _contact.id
+  },
+  {
+    icon: {
+      DataGen: '' as Asset
+    },
+    component: {
+      DataGenView: '' as AnyComponent
+    },
+    application: {
+      DataGen: '' as Ref<Application>
+    }
+  }
+)
+export default dataGenPlugin
