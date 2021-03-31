0207d85f070d465dab9411d4abba5a49f92182c6
ArtyomSavchenko <armisav@gmail.com>
2021-03-31 19:10:21 +0700

    Focus to selected date (#336)

--- packages/sparkling-controls/src/calendar/MonthCalendar.svelte
+++ packages/sparkling-controls/src/calendar/MonthCalendar.svelte
@@ -12,11 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 -->
-
 <script type="ts">
   import Button from '@anticrm/sparkling-controls/src/Button.svelte'
   import {
-    areDatesEqual, 
+    areDatesEqual,
     day,
     firstDay,
     getMonthName,
@@ -24,26 +23,55 @@
     incrementMonth,
     isWeekend,
     weekday
-  } from './internal/DateUtils';
+  } from './internal/DateUtils'
 
   export let mondayStart: boolean = false
-  export let weekFormat: "narrow" | "short" | "long" | undefined = 'short'
+  export let weekFormat: 'narrow' | 'short' | 'long' | undefined = 'short'
   export let selectedDate: Date | undefined = undefined
+  export let todayDate: Date = new Date()
 
-  let currentDate: Date = new Date()
+  let currentDate: Date = selectedDate || todayDate
   let firstDayOfCurrentMonth: Date = firstDay(currentDate, mondayStart)
 
   function onSelect(date: Date) {
-    selectedDate = date;
+    selectedDate = date
   }
 
   function incMonth(count: number) {
     if (count) {
-      currentDate = incrementMonth(currentDate, count);
-      firstDayOfCurrentMonth = firstDay(currentDate, mondayStart);
+      currentDate = incrementMonth(currentDate, count)
+      firstDayOfCurrentMonth = firstDay(currentDate, mondayStart)
     }
   }
 </script>
+
+<div class="month-calendar">
+  <div class="selected-month-controller">
+    <Button size="small" on:click={() => incMonth(-1)}>&lt;</Button>
+    <div class="month-name">{getMonthName(currentDate) + ' ' + currentDate.getFullYear()}</div>
+    <Button size="small" on:click={() => incMonth(1)}>&gt;</Button>
+  </div>
+  <div class="days-of-week-header">
+    {#each [...Array(7).keys()] as dayOfWeek}
+      <div class="day-name">{getWeekDayName(day(firstDayOfCurrentMonth, dayOfWeek), weekFormat)}</div>
+    {/each}
+  </div>
+  <div class="days-of-month">
+    {#each [...Array(6).keys()] as weekIndex}
+      {#each [...Array(7).keys()] as dayOfWeek}
+        <div
+          class="cell"
+          class:weekend={isWeekend(weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
+          class:today={areDatesEqual(todayDate, weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
+          class:selected={areDatesEqual(selectedDate, weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
+          on:click={() => onSelect(weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
+        >
+          {weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek).getDate()}
+        </div>
+      {/each}
+    {/each}
+  </div>
+</div>
 
 <style lang="scss">
   .day-name,
@@ -65,7 +93,7 @@
   .selected {
     border-radius: 3px;
     color: var(--theme-content-dark-color);
-  	background-color: var(--theme-bg-dark-color);
+    background-color: var(--theme-bg-dark-color);
   }
   .cell {
     height: 100%;
@@ -86,31 +114,3 @@
     margin: 0 5px;
   }
 </style>
-
-<div class="month-calendar">
-  <div class="selected-month-controller">
-    <Button size="small" on:click={() => incMonth(-1)}>&lt;</Button>
-    <div class="month-name">{getMonthName(currentDate) + " " + currentDate.getFullYear()}</div>
-    <Button size="small" on:click={() => incMonth(1)}>&gt;</Button>
-  </div>
-  <div class="days-of-week-header">
-    {#each [...Array(7).keys()] as dayOfWeek}
-      <div class="day-name">{getWeekDayName(day(firstDayOfCurrentMonth, dayOfWeek), weekFormat)}</div>
-    {/each}
-  </div>
-  <div class="days-of-month">
-    {#each [...Array(6).keys()] as weekIndex}
-      {#each [...Array(7).keys()] as dayOfWeek}
-        <div 
-          class="cell"
-          class:weekend={isWeekend(weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
-          class:today={areDatesEqual(new Date(), weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
-          class:selected={areDatesEqual(selectedDate, weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
-          on:click={() => onSelect(weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek))}
-        >
-          {weekday(firstDayOfCurrentMonth, weekIndex, dayOfWeek).getDate()}
-        </div>
-      {/each}
-    {/each}
-  </div>
-</div>
