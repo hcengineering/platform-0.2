ce2b4daacf53e8e8427dcce6ae50e2d6308186bb
Alexander Platov <sas_lord@mail.ru>
2021-04-11 22:51:59 +0300

    Fix ThemeSelector
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- plugins/platform-ui/src/components/internal/ThemeSelector.svelte
+++ plugins/platform-ui/src/components/internal/ThemeSelector.svelte
@@ -5,29 +5,6 @@
   import PopupItem from '@anticrm/sparkling-controls/src/menu/PopupItem.svelte'
 
   const { setTheme } = getContext('theme')
-  function changeTheme (event: any): void {
-    setTheme(event.srcElement.innerText)
-    window.removeEventListener('click', handler)
-  }
-  const themes = [..._themes]
-
-  function handler (event: MouseEvent) {
-    const cl = event.target.classList[0]
-    if (cl !== 'item' && cl !== 'menu') {
-      hidden = !hidden
-      window.removeEventListener('click', handler)
-    }
-  }
-
-  let hidden = false
-  function toggleMenu (): void {
-    hidden = !hidden
-    if (hidden) {
-      window.addEventListener('click', handler)
-    } else {
-      window.removeEventListener('click', handler)
-    }
-  }
 </script>
 
 <PopupMenu>
