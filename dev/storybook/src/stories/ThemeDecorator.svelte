e3107c566718ed49d3445002cb494454f0354e23
Alexander Platov <sas_lord@mail.ru>
2021-04-13 00:34:01 +0300

    Fix eslint
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- dev/storybook/src/stories/ThemeDecorator.svelte
+++ dev/storybook/src/stories/ThemeDecorator.svelte
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 -->
-
 <script>
   import Theme from '@anticrm/sparkling-theme/src/components/Theme.svelte'
 
