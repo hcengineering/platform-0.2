5353371989f9116f596c3f62f5cf2c463abcf174
Andrey Platov <andrey.v.platov@gmail.com>
2021-04-14 17:36:00 +0200

    it works
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- v0.4/dev/storybook/rollup.config.js
+++ v0.4/dev/storybook/rollup.config.js
@@ -5,6 +5,7 @@ import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import copy from "rollup-plugin-copy-assets";
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -43,6 +44,7 @@ export default {
   },
   plugins: [
     svelte({
+      preprocess: sveltePreprocess(),
       compilerOptions: {
         // enable run-time checks when not in production
         dev: !production,
