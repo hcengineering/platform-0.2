21e24d594e4d138b6ccd9256b38e9760222c0c9d
Alexander Platov <sas_lord@mail.ru>
2021-04-17 21:51:34 +0300

    add 'EditBox' component
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- v0.4/dev/storybook/src/stories/Button.stories.js
+++ v0.4/dev/storybook/src/stories/Button.stories.js
@@ -1,4 +1,3 @@
-
 import { Button } from '@anticrm/sparkling-controls';
 
 export default {
--- v0.4/sparkling/controls/src/index.ts
+++ v0.4/sparkling/controls/src/index.ts
@@ -14,3 +14,4 @@
 //
 
 export { default as Button } from './Button.svelte'
+export { default as EditBox } from './EditBox.svelte'
