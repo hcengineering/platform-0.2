2ab035b004628e95355b423614914e8f3f7674c0
Alexander Platov <sas_lord@mail.ru>
2021-04-22 15:46:05 +0300

    designed registration form, fix sparkling-controls, update fonts (#414)
    * designed registration form, fix sparkling-controls, update fonts
    * fix scrolling on panel
    * add and fix states in EditBox
    
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- v0.4/sparkling/controls/src/Button.svelte
+++ v0.4/sparkling/controls/src/Button.svelte
@@ -1,3 +1,18 @@
+<!--
+// Copyright © 2020 Anticrm Platform Contributors.
+// 
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 
+// See the License for the specific language governing permissions and
+// limitations under the License.
+-->
+
 <script lang="ts">
   import { createEventDispatcher } from 'svelte'
 
@@ -17,7 +32,7 @@
 
 <style lang="scss">
   .button {
-    height: 56px;
+    height: 48px;
     padding: 0;
     border: none;
     overflow: hidden;
@@ -37,8 +52,7 @@
       height: 100%;
 
       font-size: 14px;
-      line-height: 17px;
-      font-weight: 400;
+      font-weight: 600;
 
       color: var(--theme-caption-color);
       border: 1px solid var(--theme-border-accent-color);
@@ -54,7 +68,17 @@
     }
   }
 
-  .primary {
+  .primary div {
     background-color: var(--theme-primary-color);
+    border: 1px solid var(--theme-primary-border);
+    opacity: 1;
+    &:hover {
+      background-color: var(--theme-primary-color);
+      border: 1px solid var(--theme-primary-border);
+      opacity: 0.9;
+    }
+    &:active {
+      opacity: 0.8;
+    }
   }
 </style>
