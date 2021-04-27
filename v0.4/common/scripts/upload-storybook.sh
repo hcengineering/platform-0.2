70a67157a00baed0b96484309b78e7e13755dbca
Andrey Platov <andrey.v.platov@gmail.com>
2021-04-27 19:57:18 +0200

    move upload scripts to package.jsons
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- v0.4/common/scripts/upload-storybook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# storybook will be uploaded to http://anticrm-storybook.s3.us-east-2.amazonaws.com/index.html
-# http://anticrm-storybook.s3-website.us-east-2.amazonaws.com
-
-cd dev/storybook/storybook-static
-aws s3 sync . s3://anticrm-storybook --delete --acl public-read
-cd ../../..
-
---
+++ v0.4/dev/README.md
@@ -0,0 +1,6 @@
+
+
+
+* Platform - https://anticrm-platform.s3-website.us-east-2.amazonaws.com/login
+* Storybook - https://anticrm-storybook.s3-website.us-east-2.amazonaws.com
+
