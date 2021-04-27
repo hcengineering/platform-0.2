f45c9580e68690e9c4e1f59185455d6d0de545aa
Andrey Platov <andrey.v.platov@gmail.com>
2021-04-27 19:48:21 +0200

    works with s3
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- v0.4/common/scripts/upload-platform.sh
+++ v0.4/common/scripts/upload-platform.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Platfrom will be deployed to https://d1ki2i8h6jgk1u.cloudfront.net/login
+# http://anticrm-platform.s3-website.us-east-2.amazonaws.com/login
 
 cd dev/prod/dist
 aws s3 sync . s3://anticrm-platform --delete --acl public-read
