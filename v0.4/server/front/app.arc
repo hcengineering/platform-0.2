cfb99d6c5ae2e4c32c31d7d82e3c137260f18593
Andrey Platov <andrey.v.platov@gmail.com>
2021-04-29 12:08:37 +0200

    add aws `front`
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

---
+++ v0.4/server/front/app.arc
@@ -0,0 +1,9 @@
+@app
+front
+
+@http
+get /rpc
+
+# @aws
+# profile default
+# region us-west-1
---
+++ v0.4/server/front/src/http/get-rpc/index.js
@@ -0,0 +1,40 @@
+
+let mongo = require('mongodb')
+let MongoClient = mongo.MongoClient
+
+let client
+
+exports.handler = async function http (req, context) {
+
+  // See https://www.mongodb.com/blog/post/serverless-development-with-nodejs-aws-lambda-mongodb-atlas
+  // context.callbackWaitsForEmptyEventLoop = false
+  try {
+    if (client === undefined) {
+      const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/master'
+      console.log('connecting to ' + uri)
+      client = await MongoClient.connect(uri, { useUnifiedTopology: true })
+    } else {
+      console.log('reusing connection')
+    }
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+        'content-type': 'text/html; charset=utf8'
+      },
+      body: `hey there! `
+    }      
+  } catch (err) {
+    return {
+      statusCode: 200,
+      headers: {
+        'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0',
+        'content-type': 'text/html; charset=utf8'
+      },
+      body: String(err)
+    }  
+  }
+  
+  
+}
