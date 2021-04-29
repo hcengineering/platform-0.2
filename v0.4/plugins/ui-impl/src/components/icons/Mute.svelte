2a0dab3b06243a365fe4e901bb7c6380d50c08ef
Alexander Platov <sas_lord@mail.ru>
2021-04-29 21:37:46 +0300

    fix params
    Signed-off-by: Alexander Platov <sas_lord@mail.ru>

--- v0.4/plugins/ui-impl/src/components/icons/Mute.svelte
+++ v0.4/plugins/ui-impl/src/components/icons/Mute.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   export let size: number = 24
-  export let color: string = 'var(--theme-caption-color)'
+  const color: string = 'var(--theme-caption-color)'
 </script>
 
 <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
